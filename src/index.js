a1f67bb6b8930155b127971c9ca7204ae7edafae
saloni-ninjaTech <saloni@ninjatechnolabs.com>
2023-04-11 17:48:31 +0530

    added react router

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import "./index.css";
 import App from "./components/App";
 import store from "./reducers/store";
 import { Provider } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
       <App />
     </Provider>
   </React.StrictMode>
